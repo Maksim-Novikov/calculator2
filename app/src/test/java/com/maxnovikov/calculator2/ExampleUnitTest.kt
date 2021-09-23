0e1e23cd08bc277108ab5e9e821b2bcfc47edd9a
Maksim Novikov <manovikov@yotateam.com>
2021-09-23 21:31:35 +0500

    add increment test

--- app/src/test/java/com/maxnovikov/calculator2/ExampleUnitTest.kt
+++ app/src/test/java/com/maxnovikov/calculator2/ExampleUnitTest.kt
@@ -19,4 +19,12 @@ class ExampleUnitTest {
   fun multiplication_isCorrect() {
     assertEquals(4, 2 * 2)
   }
+
+  @Test
+  fun increment_isCorrect() {
+    var a = 1
+    a++
+    assertEquals(2, a)
+  }
+
 }
