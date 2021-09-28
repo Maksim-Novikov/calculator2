c10e51f0ba4990e41921e0fc1be7737a5ce76f73
Maksim Novikov <manovikov@yotateam.com>
2021-09-28 09:28:16 +0500

    activity lifecycle lesson

--- app/src/main/java/com/maxnovikov/calculator2/MainActivity.kt
+++ app/src/main/java/com/maxnovikov/calculator2/MainActivity.kt
@@ -1,5 +1,38 @@
 package com.maxnovikov.calculator2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.maxnovikov.calculator2.SettingsActivity.Companion.SETTINGS_RESULT_REQUEST_CODE
 
-class MainActivity : AppCompatActivity()
+class MainActivity : BaseActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+
+    val button: Button = findViewById(R.id.main_activity_settings)
+
+    button.setOnClickListener {
+      openSettings()
+    }
+  }
+
+  private fun openSettings() {
+    Toast.makeText(this, "Открытие настроек", Toast.LENGTH_LONG).show()
+    val intent = Intent(this, SettingsActivity::class.java)
+    intent.putExtra(SettingsActivity.SETTINGS_RESULT_KEY, 10)
+    startActivityForResult(intent, SETTINGS_RESULT_REQUEST_CODE)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    data?.getStringExtra(SettingsActivity.SETTINGS_RESULT_KEY)?.let {
+      Toast.makeText(this, "result $it", Toast.LENGTH_SHORT).show()
+    }
+  }
+}
+
+
+
