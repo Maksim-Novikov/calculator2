dc0962428e2d167b4997f4baa351e7c778a11412
Maksim Novikov <manovikov@yotateam.com>
2021-09-29 13:15:11 +0500

    layout main screen

--- app/src/main/java/com/maxnovikov/calculator2/MainActivity.kt
+++ app/src/main/java/com/maxnovikov/calculator2/MainActivity.kt
@@ -1,21 +1,55 @@
 package com.maxnovikov.calculator2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.maxnovikov.calculator2.SettingsActivity.Companion.SETTINGS_RESULT_REQUEST_CODE
+import com.maxnovikov.calculator2.databinding.MainActivityBinding
 
 class MainActivity : BaseActivity() {
+
+  private val viewBinding by viewBinding(MainActivityBinding::bind)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_activity)
 
-    val button: Button = findViewById(R.id.main_activity_settings)
-
-    button.setOnClickListener {
+    viewBinding.mainActivitySettings.setOnClickListener {
       openSettings()
+    }
+
+    listOf(
+      viewBinding.mainZero,
+      viewBinding.mainOne,
+      viewBinding.mainTwo,
+      viewBinding.mainThree,
+      viewBinding.mainFour,
+      viewBinding.mainFive,
+      viewBinding.mainSix,
+      viewBinding.mainSeven,
+      viewBinding.mainEight,
+      viewBinding.mainNine,
+    ).forEachIndexed { index, textView ->
+      textView.setOnClickListener { onNumberClick(index) }
+    }
+
+    viewBinding.mainBack.setOnClickListener {
+      removeLast()
+    }
+  }
+
+  @SuppressLint("SetTextI18n")
+  private fun onNumberClick(number: Int) {
+    with(viewBinding.mainSecondArgument) {
+      text = text.toString() + number.toString()
+    }
+  }
+
+  private fun removeLast() {
+    with(viewBinding.mainSecondArgument) {
+      text = text.dropLast(1)
     }
   }
 
